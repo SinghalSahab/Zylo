4a969b55c03df90d5e25fa3130c2092392852f7b
SinghalSahab <prakharsinghal10a@gmail.com>
2025-01-06 01:41:09 +0530

    start page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Routes , Route} from 'react-router-dom'
+import {CaptainLogin, CaptainSignup, Start, UserLogin, UserSignup } from './pages'
 
 const App = () => {
   return (
-    <div className='text-blue-200'><h1>vszdgvf</h1></div>
+    <div className=''>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/users/login' element={<UserLogin />} />
+        <Route path='/users/register' element={<UserSignup />} />
+        <Route path='/captains/login' element={<CaptainLogin />} />
+        <Route path='/captains/register' element={<CaptainSignup />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Start } from './Start';
+export { default as UserLogin } from './UserLogin';
+export { default as UserSignup } from './UserSignup';
+export { default as CaptainSignup } from './CaptainSignup';
+export { default as CaptainLogin } from './CaptainLogin';
