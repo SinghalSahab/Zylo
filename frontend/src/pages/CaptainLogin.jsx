f330f68b92688bd73739aff0101fcbfbd70ac476
SinghalSahab <prakharsinghal10a@gmail.com>
2025-01-13 03:59:24 +0530

    user signup

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -29,7 +29,7 @@ const CaptainLogin = () => {
           
         }} className='flex flex-col justify-start'>
 
-            <h3 className='text-3xl mb-3'>What's your email adress</h3>
+            <h3 className='text-3xl font-medium mb-3'>What's your email adress</h3>
             <input 
             required 
             value = {email}
@@ -39,7 +39,7 @@ const CaptainLogin = () => {
             type="email"
             className='bg-[#eeeeee] mb-5 rounded-xl text-lg placeholder:text-base px-4 py-2  w-80 border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none '
              placeholder='Enter your email '/>
-            <h3 className='text-3xl mb-3'>Enter your password</h3>
+            <h3 className='text-3xl font-medium mb-3'>Enter your password</h3>
             <input
             required 
             value = {password}
--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,44 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/Logo.png'
+import {Link} from 'react-router-dom'
 
 const UserSignup = () => {
   return (
-    <div>
-      <div className='p-7 h-screen flex flex-col justify-between'>
+    <div className=''>
+        
+        <img className='w-28 mb-10 ml-5' src={Logo} alt="" />
+        <div className='flex flex-col justify-between h-[43vw] items-center'>
         <div>
-           <img className='w-28 mb-10 ml-5' src={Logo} alt="" />
-           <form >
-            <h3 className='text-2xl w-1/2  font-medium mb-2'>What's your name</h3>
-            <div className='flex gap-4 mb-6'>
-              <input 
-              type="text"
-              required
-              placeholder='Enter your first name'
-              className='bg-[#eeeeee] rounded-xl  w-52 px-3 py-2 placeholder:text-base border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none' />
-              <input
-               type="text"
-               placeholder='Enter your last name'
-               className='bg-[#eeeeee] rounded-xl w-52 px-3 py-2 placeholder:text-base border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none' />
-            </div>
-            <h3 className='text-2xl w-1/2  font-medium mb-2'>What's your email address</h3>
-            <input 
-            type="email"
-            required
-            placeholder='Enter your email'
-            className='bg-[#eeeeee] rounded-xl w-52 mb-6 px-3 py-2 placeholder:text-base border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none' />
-            <h3 className='text-2xl w-1/2  font-medium mb-2'>Enter your password</h3>
-            <input 
+        <form className='flex flex-col justify-start'>
+
+            <h3 className='text-3xl font-medium mb-3'>What's your name</h3>
+            <div className='gap-4 flex mb-5'>
+            <input
+            required 
+            
+            type="text"
+            className='bg-[#eeeeee] rounded-xl text-lg placeholder:text-base px-4 py-2 w-1/2 border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none'
+             placeholder='First name' />
+             <input
+            required 
+            
             type="password"
-            required
-            placeholder='Enter your password'
-            className='bg-[#eeeeee] rounded-xl w-52 mb-6 px-3 py-2 placeholder:text-base border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none' />
-<br/>
-           <button className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-80 text-lg placeholder:text-base'>
-            Create account
+            className='bg-[#eeeeee] rounded-xl text-lg placeholder:text-base px-4 py-2  w-1/2 border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none'
+             placeholder='Last name' />
+            </div>
+            <h3 className='text-3xl font-medium mb-3'>What's your email adress</h3>
+            <input 
+            required 
+            
+            type="text"
+            className='bg-[#eeeeee] mb-5 rounded-xl text-lg placeholder:text-base px-4 py-2  w-80 border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none '
+             placeholder='Enter your email '/>
+            <h3 className='text-3xl font-medium mb-3'>Enter your password</h3>
+            <input
+            required 
+            
+            type="password"
+            className='bg-[#eeeeee] mb-5 rounded-xl text-lg placeholder:text-base px-4 py-2  w-80 border-2 border-[#eeeeee] focus:border-zinc-950 focus:outline-none'
+             placeholder='password' />
+            <button 
+            className='bg-[#111] mb-1 rounded-xl text-white font-semibold placeholder:text-base px-4 py-2  w-80 '
+            >
+                Login
             </button>
-           </form>
+        </form>
+        
+        <div>
+          New here? <Link to='/users/register' className='text-blue-600  font-medium'>Create new account</Link>
         </div>
-      </div>
+        </div>
+        <div>
+        <Link
+          to='/captains/login'
+          className='bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-80 text-lg placeholder:text-base'
+        >Sign in as Captain</Link>
+        </div>
+        
+        </div>
+        
     </div>
   )
 }
