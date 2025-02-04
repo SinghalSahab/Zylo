9dfff0ad834e22babd08ffd5579aa8057c422ea2
SinghalSahab <prakharsinghal10a@gmail.com>
2025-02-04 23:27:15 +0530

    c

--- frontend/src/components/ConfirmRide.jsx
+++ frontend/src/components/ConfirmRide.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import PlaceIcon from '@mui/icons-material/Place';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-const ConfirmRide = () => {
+const ConfirmRide = (props) => {
   return (
     <div>
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
@@ -11,12 +12,29 @@ const ConfirmRide = () => {
             <div className='flex gap-2 justify-between flex-col items-center'>
                 <img className='w-1/3' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png" alt="" />
                 <div className='w-full'>
+                     <div className='flex items-center gap-2'>
                      <div>
-                        <h1>562/11-A</h1>
-                        <h2>Muradnagar,Ghaziabad ,U.P</h2>
+                           <PlaceIcon />
+                       </div>
+                       <div>
+                       <h1 className='text-xl font-semibold'>562/11-A</h1>
+                       <h2 className='text-base text-gray-600'>Muradnagar,Ghaziabad ,U.P</h2>
+                       </div>
                      </div>
+                     <div>
+                       <div></div>
+                       <div>
+                       <h1 className='text-xl font-semibold'>Third Wave Coffee</h1>
+                       <h2 className='text-base text-gray-600'>Muradnagar,Ghaziabad ,U.P</h2>
+                       </div>
+                     </div>
+                     <div>
                      <div></div>
-                     <div></div>
+                       <div>
+                       <h1 className='text-xl font-semibold'>₹193.20</h1>
+                       <h2 className='text-base text-gray-600'>Cash</h2>
+                       </div>
+                     </div>
                 </div>
                 <button className='w-full'>Confirm</button>
             </div>
